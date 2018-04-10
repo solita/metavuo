c3a4b5fe6463a9325a54b751b384670f4da71947
Esko Oramaa <esko.oramaa@solita.fi>
2018-04-10 19:38:57 +0300

    Create initial directory structure
    Closes #23

---
+++ backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	http.HandleFunc("/api/", routeApi)
+}
+
+func routeApi(w http.ResponseWriter, r *http.Request) {
+
+	if strings.HasPrefix(r.URL.Path, "/api/admin/") {
+		fmt.Fprintf(w, "Hello Admin!")
+		return
+	}
+
+	if strings.HasPrefix(r.URL.Path, "/api/") {
+		fmt.Fprintf(w, "Hello")
+		return
+	}
+
+	http.Error(w, "", http.StatusNotFound)
+}
